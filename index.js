e72706632f7b6950a7130e3f253836183ad22928
Jose Luis Escanciano <jlescanciano@gmail.com>
2020-05-05 16:31:45 +0200

    using await

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `repo-token` input defined in action metadata file
-  const repositoryToken = core.getInput('repo-token');
-  const nameToGreet = core.getInput('who-to-greet');
-  const octokit = new github.GitHub(repositoryToken);
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+async function run() {
+  try {
+    // `repo-token` input defined in action metadata file
+    const repositoryToken = core.getInput('repo-token');
+    const nameToGreet = core.getInput('who-to-greet');
+    const octokit = new github.GitHub(repositoryToken);
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
 
-  let result = octokit.pulls.listFiles({owner, repo, pull_number: github.context.payload.number});
-  console.log(JSON.stringify(result, undefined, 2));
-  let changedFiles = result.data.map(file => file.filename);
-  console.log(`The files changed: ${changedfiles}`);
+    let result = await octokit.pulls.listFiles({owner, repo, pull_number: github.context.payload.number});
+    console.log(JSON.stringify(result, undefined, 2));
+    let changedFiles = result.data.map(file => file.filename);
+    console.log(`The files changed: ${changedfiles}`);
 
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
