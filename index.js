dfb32f8b06e6a2071d06be254579406246e78a42
Jose Luis Escanciano <jlescanciano@gmail.com>
2020-05-05 16:03:41 +0200

    fix repo token

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
+  // `repo-token` input defined in action metadata file
+  const repositoryToken = core.getInput('repo-token');
   const nameToGreet = core.getInput('who-to-greet');
+  const octokit = new github.GitHub(repositoryToken);
+
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  const myToken = core.getInput('myToken');
-  const octokit = new github.GitHub(myToken);
+  
 } catch (error) {
   core.setFailed(error.message);
 }
