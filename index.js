d36139e0eb5a95a938b8668ae1c5a018b19b15d9
Jose Luis Escanciano <jlescanciano@gmail.com>
2020-05-05 16:20:27 +0200

    log changed files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ try {
   const repositoryToken = core.getInput('repo-token');
   const nameToGreet = core.getInput('who-to-greet');
   const octokit = new github.GitHub(repositoryToken);
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
@@ -13,7 +14,11 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  
+
+  let result = octokit.pulls.listFiles({owner, repo, pull_number: payload.number})
+  let changedFiles = result.data.map(file => file.filename)
+  console.log(`The files changed: ${changedfiles}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
