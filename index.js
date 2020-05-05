cb6cac4aa930725f47dfd46847f0daa189666e9e
Jose Luis Escanciano <jlescanciano@gmail.com>
2020-05-05 15:56:03 +0200

    token param

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const myToken = core.getInput('myToken');
+  const octokit = new github.GitHub(myToken);
 } catch (error) {
   core.setFailed(error.message);
 }
