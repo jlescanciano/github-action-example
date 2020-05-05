bece57755093de7db0c14593db5d55633ad50891
Jose Luis Escanciano <jlescanciano@gmail.com>
2020-05-05 16:40:26 +0200

    typo

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ async function run() {
     const nameToGreet = core.getInput('who-to-greet');
     const octokit = new github.GitHub(repositoryToken);
 
-
-    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
@@ -24,7 +21,7 @@ async function run() {
       pull_number: github.context.payload.number});
     console.log(JSON.stringify(result, undefined, 2));
     let changedFiles = result.data.map(file => file.filename);
-    console.log(`The files changed: ${changedfiles}`);
+    console.log(`The files changed: ${changedFiles}`);
 
   } catch (error) {
     core.setFailed(error.message);
